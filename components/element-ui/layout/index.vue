9d0f20bfc0e8c2e83fb4f5121ed84ff17187b91a
Leon <leon@haha.com>
2020-01-06 14:55:49 +0800

    添加layout实例；修改row缺少清楚浮动的bug

--- components/element-ui/layout/index.vue
+++ components/element-ui/layout/index.vue
@@ -1,56 +1,171 @@
 <template>
   <div>
     <div>
-      <el-row>
-        <el-col :span="24"
-          ><div class="grid-content bg-purple-dark"></div
-        ></el-col>
-      </el-row>
-      <el-row>
-        <el-col :span="12"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="12"
-          ><div class="grid-content bg-purple-light"></div
-        ></el-col>
-      </el-row>
-      <el-row>
-        <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="8"
-          ><div class="grid-content bg-purple-light"></div
-        ></el-col>
-        <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
-      </el-row>
-      <el-row>
-        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="6"
-          ><div class="grid-content bg-purple-light"></div
-        ></el-col>
-        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="6"
-          ><div class="grid-content bg-purple-light"></div
-        ></el-col>
-      </el-row>
-      <el-row>
-        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="4"
-          ><div class="grid-content bg-purple-light"></div
-        ></el-col>
-        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="4"
-          ><div class="grid-content bg-purple-light"></div
-        ></el-col>
-        <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :span="4"
-          ><div class="grid-content bg-purple-light"></div
-        ></el-col>
-      </el-row>
+      <h3>基础布局</h3>
+      <p>使用单一分栏创建基础的栅格布局。</p>
+      <div>
+        <el-row>
+          <el-col :span="24"
+            ><div class="grid-content bg-purple-dark"></div
+          ></el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="12"><div class="grid-content bg-purple"></div></el-col>
+          <el-col :span="12"
+            ><div class="grid-content bg-purple-light"></div
+          ></el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+          <el-col :span="8"
+            ><div class="grid-content bg-purple-light"></div
+          ></el-col>
+          <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+          <el-col :span="6"
+            ><div class="grid-content bg-purple-light"></div
+          ></el-col>
+          <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+          <el-col :span="6"
+            ><div class="grid-content bg-purple-light"></div
+          ></el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+          <el-col :span="4"
+            ><div class="grid-content bg-purple-light"></div
+          ></el-col>
+          <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+          <el-col :span="4"
+            ><div class="grid-content bg-purple-light"></div
+          ></el-col>
+          <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+          <el-col :span="4"
+            ><div class="grid-content bg-purple-light"></div
+          ></el-col>
+        </el-row>
+      </div>
     </div>
 
     <div>
-      <el-row :gutter="20">
+      <h3>分栏间隔</h3>
+      <p>分栏之间存在间隔。</p>
+      <div>
+        <el-row :gutter="20">
+          <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+          <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+          <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+          <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+        </el-row>
+      </div>
+    </div>
+
+    <div>
+      <h3>混合布局</h3>
+      <p>通过基础的 1/24 分栏任意扩展组合形成较为复杂的混合布局。</p>
+      <div>
+        <el-row :gutter="20">
+          <el-col :span="16"><div class="grid-content bg-purple"></div></el-col>
+          <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+          <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+          <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+          <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+          <el-col :span="16"><div class="grid-content bg-purple"></div></el-col>
+          <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+        </el-row>
+      </div>
+    </div>
+    <div>
+      <h3>分栏偏移</h3>
+      <p>支持偏移指定的栏数。</p>
+      <div>
+        <el-row :gutter="20">
+          <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+          <el-col :span="6" :offset="6"
+            ><div class="grid-content bg-purple"></div
+          ></el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="6" :offset="6"
+            ><div class="grid-content bg-purple"></div
+          ></el-col>
+          <el-col :span="6" :offset="6"
+            ><div class="grid-content bg-purple"></div
+          ></el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="12" :offset="6"
+            ><div class="grid-content bg-purple"></div
+          ></el-col>
+        </el-row>
+      </div>
+    </div>
+    <div>
+      <h3>对齐方式</h3>
+      <p>通过 flex 布局来对分栏进行灵活的对齐。</p>
+      <el-row type="flex" class="row-bg">
         <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="6"
+          ><div class="grid-content bg-purple-light"></div
+        ></el-col>
         <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+      </el-row>
+      <el-row type="flex" class="row-bg" justify="center">
         <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="6"
+          ><div class="grid-content bg-purple-light"></div
+        ></el-col>
         <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+      </el-row>
+      <el-row type="flex" class="row-bg" justify="end">
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="6"
+          ><div class="grid-content bg-purple-light"></div
+        ></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+      </el-row>
+      <el-row type="flex" class="row-bg" justify="space-between">
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="6"
+          ><div class="grid-content bg-purple-light"></div
+        ></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+      </el-row>
+      <el-row type="flex" class="row-bg" justify="space-around">
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="6"
+          ><div class="grid-content bg-purple-light"></div
+        ></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+      </el-row>
+    </div>
+    <div>
+      <h3>响应式布局</h3>
+      <p>
+        参照了 Bootstrap 的 响应式设计，预设了五个响应尺寸：xs、sm、md、lg 和
+        xl。
+      </p>
+      <el-row :gutter="10">
+        <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1"
+          ><div class="grid-content bg-purple"></div
+        ></el-col>
+        <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"
+          ><div class="grid-content bg-purple-light"></div
+        ></el-col>
+        <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"
+          ><div class="grid-content bg-purple"></div
+        ></el-col>
+        <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1"
+          ><div class="grid-content bg-purple-light"></div
+        ></el-col>
       </el-row>
     </div>
   </div>
